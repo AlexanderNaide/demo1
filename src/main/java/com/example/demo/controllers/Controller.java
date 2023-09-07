a9e785c21c7beb8bea226f051e4160a55bb1465f
Alexandr <kb_299@bk.ru>
2023-09-07 14:11:42 +0300

    ***

--- src/main/java/com/example/demo/controllers/Controller.java
+++ src/main/java/com/example/demo/controllers/Controller.java
@@ -4,13 +4,9 @@ import com.example.demo.entities.User;
 import com.example.demo.model.RequestCreateUser;
 import com.example.demo.model.ResponseNewUser;
 import com.example.demo.services.UserService;
-import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 import java.util.Objects;
 import java.util.stream.Stream;
 
